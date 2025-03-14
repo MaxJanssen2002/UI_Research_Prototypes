c0cd96c63ee79c700012c881d4005d73c8ebe53c
gianninipruijn <g.pruijn@student.han.nl>
2025-03-14 10:08:42 +0100

    overlay

--- Tankey Online/Assets/Scripts/InventoryManager.cs
+++ Tankey Online/Assets/Scripts/InventoryManager.cs
@@ -10,7 +10,7 @@ public class InventoryManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InventoryMenu.SetActive(false);
     }
 
     // Update is called once per frame
