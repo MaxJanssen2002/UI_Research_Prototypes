969bf3976c819aea29eab18a44b31889afe22cfd
Max Janssen <M.Janssen40@student.han.nl>
2025-03-19 11:09:39 +0100

    Added object spawner

--- Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
+++ Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody rb;
+    public GameObject objectToThrow;
     public float movementSpeed;
     public float rotationSpeed;
 
     
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
         movementSpeed = 2.0f;
         rotationSpeed = 120.0f;
     }
     
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
             rb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime);
@@ -30,5 +31,14 @@ public class PlayerScript : MonoBehaviour
             Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
             rb.MoveRotation(transform.rotation * deltaRotation);
         }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            throwObject();
+        }
+    }
+
+
+    private void throwObject() {
+        GameObject newObject = Instantiate(objectToThrow, transform.position + transform.forward * 0.5f, Quaternion.identity);
+        newObject.name = "Sphere";
     }
 }
