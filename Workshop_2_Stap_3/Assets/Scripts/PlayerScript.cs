c2e0ea76e4f037c2a49b4a569684575aac918e88
Max Janssen <M.Janssen40@student.han.nl>
2025-03-19 10:32:58 +0100

    Added rigidbody and collision

--- Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
+++ Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
@@ -2,25 +2,33 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    private Rigidbody rb;
     public float movementSpeed;
     public float rotationSpeed;
 
     
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         movementSpeed = 2.0f;
         rotationSpeed = 120.0f;
     }
     
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime, Space.Self);
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(Vector3.forward * -movementSpeed * Time.deltaTime, Space.Self);
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(transform.TransformDirection(transform.up) * rotationSpeed * Time.deltaTime, Space.Self); 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(transform.TransformDirection(transform.up) * -rotationSpeed * Time.deltaTime, Space.Self);  
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            rb.MovePosition(transform.position + transform.forward * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            rb.MovePosition(transform.position + -transform.forward * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, -rotationSpeed, 0) * Time.deltaTime);
+            rb.MoveRotation(transform.rotation * deltaRotation);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+            rb.MoveRotation(transform.rotation * deltaRotation);
+        }
     }
 }
