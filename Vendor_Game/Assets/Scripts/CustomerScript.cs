854ee0a626426463d7f8298d376f42e580031695
Max Janssen <M.Janssen40@student.han.nl>
2025-03-26 11:44:37 +0100

    Added variables to customer

---
+++ Vendor_Game/Assets/Scripts/CustomerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CustomerScript : MonoBehaviour
+{
+    public enum Role { Buyer, Seller };
+
+    public Role role;
+    public int price;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
