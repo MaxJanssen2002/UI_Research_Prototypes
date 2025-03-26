15ba67a00e8ac774f6a20ba7662340d0949dc1ef
Max Janssen <M.Janssen40@student.han.nl>
2025-03-26 09:42:36 +0100

    Removed old objects

--- Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
+++ Workshop_2_Stap_3/Assets/Scripts/PlayerScript.cs
@@ -3,12 +3,8 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody rb;
-    public GameObject objectToThrow;
     public float movementSpeed;
     public float rotationSpeed;
-    private float objectSpawnDistance;
-    private float throwingForceForward;
-    private float throwingForceUp;
 
     
     private void Start()
@@ -16,9 +12,6 @@ public class PlayerScript : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         movementSpeed = 2.0f;
         rotationSpeed = 120.0f;
-        objectSpawnDistance = 0.5f;
-        throwingForceForward = 300.0f;
-        throwingForceUp = 150.0f;
     }
     
     private void FixedUpdate()
@@ -36,21 +29,6 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
             Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
             rb.MoveRotation(transform.rotation * deltaRotation);
-        }
-        if (Input.GetKeyDown(KeyCode.E)) {
-            throwObject();
-        }
-    }
-
-
-    private void throwObject() {
-        GameObject newObject = Instantiate(objectToThrow, transform.position + transform.forward * objectSpawnDistance, Quaternion.identity);
-        newObject.name = "Metal_Ball";
-
-        Rigidbody thrownObjectRb = newObject.GetComponent<Rigidbody>();
-        if (thrownObjectRb) {
-            thrownObjectRb.AddForce(transform.forward * throwingForceForward, ForceMode.Force);
-            thrownObjectRb.AddForce(transform.up * throwingForceUp, ForceMode.Force);
         }
     }
 }
