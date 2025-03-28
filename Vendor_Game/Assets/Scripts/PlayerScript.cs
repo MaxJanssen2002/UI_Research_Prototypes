305429ff596c44b366c91eeeb232ddb2b0a10192
Max Janssen <M.Janssen40@student.han.nl>
2025-03-28 09:43:11 +0100

    Changed behavior of customer

--- Vendor_Game/Assets/Scripts/PlayerScript.cs
+++ Vendor_Game/Assets/Scripts/PlayerScript.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
+using UnityEditor;
 
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody rb;
+    private WorldInfo worldInfo;
     public float movementSpeed;
     public float rotationSpeed;
 
+    private float targetCustomerDistance;
     private float rayDistance;
     private float maxRayDistance;
     private Ray shootingRay;
-    private RaycastHit raycastHit;
+    private RaycastHit hit;
 
     
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        LoadWorldInfo();
+        targetCustomerDistance = worldInfo.targetCustomerDistance;
         movementSpeed = 2.0f;
         rotationSpeed = 120.0f;
         maxRayDistance = 100.0f;
@@ -43,12 +48,23 @@ public class PlayerScript : MonoBehaviour
     }
 
 
+    private void LoadWorldInfo() {
+        worldInfo = AssetDatabase.LoadAssetAtPath<WorldInfo>("Assets/WorldInfo.asset");
+        if (!worldInfo) {
+            worldInfo = ScriptableObject.CreateInstance<WorldInfo>();
+            AssetDatabase.CreateAsset(worldInfo, "Assets/WorldInfo.asset");
+        }
+    }
+
+
     private void InteractWithCustomer() {
         shootingRay = new Ray (transform.position, transform.TransformDirection(Vector3.forward));
-        if (Physics.Raycast(shootingRay.origin, transform.TransformDirection(Vector3.forward), out raycastHit, rayDistance)) {
-            rayDistance = raycastHit.distance;
-            if (raycastHit.transform.gameObject.tag == "Customer") {
-                Debug.Log("Interacting with customer");
+        if (Physics.Raycast(shootingRay.origin, transform.TransformDirection(Vector3.forward), out hit, rayDistance)) {
+            rayDistance = hit.distance;
+            if (hit.transform.gameObject.tag == "Customer") {
+                if (Vector3.Distance(transform.position, hit.transform.position) <= targetCustomerDistance) {
+                    Debug.Log("Interacting with customer");
+                }
             }
         }
         else {
---
+++ Vendor_Game/Assets/Scripts/WorldInfo.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WorldInfo : ScriptableObject
+{
+    public float targetCustomerDistance = 1.5f;
+}
