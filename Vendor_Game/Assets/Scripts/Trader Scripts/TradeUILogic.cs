4ff466ffa3366096db5e4155ced1e5eb94e334a0
gianninipruijn <g.pruijn@student.han.nl>
2025-03-30 15:18:55 +0200

    TRade Ui logica voor kopen van items en toevegen aan tijdelijk inventory

--- Vendor_Game/Assets/Scripts/Trader Scripts/TradeUILogic.cs
+++ Vendor_Game/Assets/Scripts/Trader Scripts/TradeUILogic.cs
@@ -1,16 +1,75 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TradeUILogic : MonoBehaviour
+public class TradeUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Button buyButton;
+    public Button closeButton;
+
+    private CustomerScript currentSeller;
+    private PlayerScript player;
+
+    public void OpenTrade(CustomerScript seller, PlayerScript playerScript)
     {
-        
+        currentSeller = seller;
+        player = playerScript;
+
+        // Update UI dynamically based on seller and player data
+        UpdateShop();
+        UpdateEmeraldDisplay();
+
+        // Show UI
+        gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateShop()
     {
-        
+        GameObject itemPrefab = currentSeller.GetItemForSale();
+        int price = currentSeller.GetPrice();
+
+        if (itemPrefab != null)
+        {
+            itemNameText.text = itemPrefab.name;
+            priceText.text = $"Price: {price} Emeralds";
+
+            buyButton.onClick.RemoveAllListeners();
+            buyButton.onClick.AddListener(() => TryBuyItem(itemPrefab, price));
+            buyButton.interactable = true;
+        }
+        else
+        {
+            itemNameText.text = "No item available";
+            priceText.text = "";
+            buyButton.interactable = false;
+        }
+    }
+
+    void TryBuyItem(GameObject itemPrefab, int price)
+    {
+        if (player.CanAfford(price))
+        {
+            player.SpendEmeralds(price); 
+            player.Inventory.Add( itemPrefab);
+            UpdateEmeraldDisplay();
+            Debug.Log($"Bought {itemPrefab.name} for {price} emeralds.");
+        }
+        else
+        {
+            Debug.Log("Not enough emeralds!");
+        }
+    }
+
+    void UpdateEmeraldDisplay()
+    {
+    
+    }
+
+    void Start()
+    {
+        closeButton.onClick.AddListener(() => gameObject.SetActive(false));
+        gameObject.SetActive(false);
     }
 }
